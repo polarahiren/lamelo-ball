0f12b382374d65f8b7970d0eae9e752b7baacc5c
polara Hiren <hiren.polara22@gmail.com>
2022-01-23 16:35:03 +0530

    hero slider improved

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,22 @@ try {
     window.$ = window.jQuery = require('jquery')
   } catch (e) {}
   
+  require('slick-carousel')
   
   $(document).ready(function(){
     $(".humburg-menu").click(function(){
       $(".download-buttons").slideToggle(200);
       $(".download-buttons").toggleClass('show');
+    });
+
+    //hero slider
+    $('.hero-slider').slick({
+      autoplay: true,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
     });
   });  
